d525a11e92afc274b6ddc2ee6f93a1707ac8a1db
Adam McElwee <adam@mcelwee.me>
2015-02-15 19:20:23 -0600

    Remove the unnecessary listener registrations and unnecessary rebalance calls

--- recipes/distributed-lock-manager/src/main/java/org/apache/helix/lockmanager/LockProcess.java
+++ recipes/distributed-lock-manager/src/main/java/org/apache/helix/lockmanager/LockProcess.java
@@ -19,19 +19,17 @@ package org.apache.helix.lockmanager;
  * under the License.
  */
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Function;
-
-import com.google.common.base.Joiner;
-import org.apache.helix.*;
+import org.apache.helix.HelixManager;
+import org.apache.helix.HelixManagerFactory;
+import org.apache.helix.InstanceType;
+import org.apache.helix.NotificationContext;
 import org.apache.helix.api.id.StateModelDefId;
 import org.apache.helix.controller.HelixControllerMain;
 import org.apache.helix.manager.zk.ZKHelixAdmin;
-import org.apache.helix.model.CurrentState;
 import org.apache.helix.model.InstanceConfig;
-import org.apache.helix.model.LiveInstance;
-import org.apache.helix.model.Message;
+
+import java.util.List;
+import java.util.function.Function;
 
 public class LockProcess {
   private String clusterName;
@@ -69,10 +67,6 @@ public class LockProcess {
         StateModelDefId.from("OnlineOffline"), lockFactory);
     participantManager.connect();
 
-    addLiveInstanceChangeListener();
-    addMessageListener();
-    addInstanceConfigChangeListener();
-
     System.out.println("STARTED " + instanceName);
 
   }
@@ -96,78 +90,6 @@ public class LockProcess {
     return new LockFactory(lockAcquiredHandler, lockReleasedHandler);
   }
 
-  private void addLiveInstanceChangeListener() throws Exception {
-//    participantManager.addCurrentStateChangeListener(new CurrentStateChangeListener() {
-//      @Override
-//      public void onStateChange(String instanceName, List<CurrentState> statesInfo, NotificationContext changeContext) {
-//        helixAdmin.rebalance(clusterName, resourceName, 1);
-//
-//        Function<CurrentState, String> mapper = new Function<CurrentState, String>() {
-//          @Override
-//          public String apply(CurrentState currentState) {
-//            return Joiner.on(',').withKeyValueSeparator("=").join(currentState.getPartitionStateMap());
-//          }
-//        };
-//        System.out.println(String.format(
-//            "CurrentStateChangeListener: instanceName - %s  -- statesInfo - %s  --  changeContext - %s",
-//            instanceName,
-//            Joiner.on(',').join(statesInfo.stream().map(mapper).toArray()),
-//            "foo"
-//        ));
-//      }
-//    }, instanceName, participantManager.getSessionId());
-    participantManager.addLiveInstanceChangeListener(new LiveInstanceChangeListener() {
-      @Override
-      public void onLiveInstanceChange(List<LiveInstance> liveInstances, NotificationContext changeContext) {
-        // Hmm, so much for "auto rebalance"
-        helixAdmin.rebalance(clusterName, resourceName, 1);
-
-        Function<LiveInstance, String> mapper = new Function<LiveInstance, String>() {
-          @Override
-          public String apply(LiveInstance liveInstance) {
-            return String.format("%s - %s", liveInstance.getParticipantId(), liveInstance.getInstanceName());
-          }
-        };
-        System.out.println(String.format(
-            "LiveInstanceChangeListener: liveInstances - %s   --  changeContext - %s",
-            Joiner.on(',').join(liveInstances.stream().map(mapper).toArray()),
-            notificationContextToString(changeContext)
-        ));
-      }
-    });
-
- }
-
-  private void addInstanceConfigChangeListener() throws Exception {
-    participantManager.addInstanceConfigChangeListener(new InstanceConfigChangeListener() {
-      @Override
-      public void onInstanceConfigChange(List<InstanceConfig> instanceConfigs, NotificationContext context) {
-
-      }
-    });
-  }
-
-  private void addMessageListener() throws Exception {
-    participantManager.addMessageListener(new MessageListener() {
-      @Override
-      public void onMessage(String instanceName, List<Message> messages, NotificationContext changeContext) {
-
-        Function<Message, String> mapper = new Function<Message, String>() {
-          @Override
-          public String apply(Message message) {
-            return "";
-          }
-        };
-        System.out.println(String.format(
-            "MessageListener: instanceName - %s  --  messages - %s  --  changeContext - %s",
-            instanceName,
-            Joiner.on(',').join(messages.stream().map(mapper).toArray()),
-            notificationContextToString(changeContext)
-        ));
-      }
-    }, instanceName);
-  }
-
   private void startController() {
     controllerManager =
         HelixControllerMain.startHelixController(zkAddress, clusterName, "controller",
@@ -188,10 +110,7 @@ public class LockProcess {
     }
   }
 
-  public void stop(InstanceConfig instanceConfig) {
-    // TODO - work is never reclaimed unless I explicitly call this. Ideally, a failed node's work would be auto reclaimed
-//    helixAdmin.dropInstance(clusterName, instanceConfig);
-
+  public void stop() {
     if (participantManager != null) {
       participantManager.disconnect();
     }
@@ -219,7 +138,7 @@ public class LockProcess {
       @Override
       public void run() {
         System.out.println("Shutting down " + instanceName);
-        lockProcess.stop(instanceConfig);
+        lockProcess.stop();
       }
     });
     lockProcess.start(instanceConfig);
